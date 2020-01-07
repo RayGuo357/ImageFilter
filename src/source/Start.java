09c2d6345b66c044c07990fb81d5c8e9bb670bab
Raymond Guo <rmguo@live.unc.edu>
2020-01-07 18:11:12 -0500

    Better feel

--- src/source/Start.java
+++ src/source/Start.java
@@ -2,10 +2,20 @@ package source;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class Start extends JFrame {
 
@@ -37,6 +47,38 @@ public class Start extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("New button");
+		contentPane.add(btnNewButton, BorderLayout.SOUTH);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+		
+		JLabel image = new JLabel();
+		ImageIcon nImage = new ImageIcon("NeverBetter.png");
+		image.setIcon(new ImageIcon(resizeImage(nImage.getImage(), 450, 300)));
+		contentPane.add(image, BorderLayout.CENTER);
+		
+		
+				
+		btnNewButton.addActionListener(new ActionListener() {
+			int i = 0;
+			public void actionPerformed(ActionEvent e) {
+				lblNewLabel.setText(i + "");
+				i++;
+			}
+		});
+	}
+	
+	public Image resizeImage(Image icon, int w, int h) {
+		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D g2 = resizedImg.createGraphics();
+
+	    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+	    g2.drawImage(icon, 0, 0, w, h, null);
+	    g2.dispose();
+
+	    return resizedImg;
 	}
 
 }
